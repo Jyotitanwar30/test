21afaf08f79389b25daad727fbf8e6cce57ba685
Jyoti Tanwar <99900588+Jyotitanwar30@users.noreply.github.com>
2022-03-25 16:17:08 +0530

    Update OurTeam.js

--- OurTeam.js
+++ OurTeam.js
@@ -3,45 +3,22 @@ const ourteam =
 [
     {
         id : 1,
-        imgscr : "/kamb.jpeg",
-        name : "Kamb K Sinha",
-        info : "President"
+        imgscr : "/",
+        name : "",
+        info : ""
     },
     {
         id : 2,
-        imgscr : "./ashutoshd.JPG",
-        name : "Ashutosh Kr Dwivedi",
-        info : "Secretary"
+        imgscr : "/",
+        name : "",
+        info : ""
     },
     {
         id : 3,
-        imgscr : "",
-        name : "Anubhav Kant Mishra",
-        info : "Treasurer"
+        imgscr : "/",
+        name : "",
+        info : ""
     },
-    {
-        id : 4,
-        imgscr : "",
-        name : "Kamlesh Kr Dadsena",
-        info : "Vice President"
-    },
-    {
-        id : 5,
-        imgscr : "",
-        name : "Riya Singh",
-        info : "Joint Secretary"
-    },
-    {
-        id : 6,
-        imgscr : "",
-        name : "Ashutosh Mishra (Advocate)",
-        info : "Legal Advisor"
-    },
-    {
-        id : 7,
-        imgscr : "",
-        name : "Dinesh Tiwari",
-        info : "Chronologer"
-    }
+    
 ];
 export default ourteam;
